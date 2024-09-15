0ca2334187fe8b23b57c5fca5e8b52200b8f24ff
AnandaRAFIDISON <anandarafidison@gmail.com>
2024-09-15 16:50:06 +0300

    Se connecter avec Mysql

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const mysql = require("mysql");
 const port = 5000;
 
 const app = express();
 
+/*
 app.get("/", (req, res) => {
   res.send("Hello world avec node.js ");
 });
 
 const users = require("./routes/users");
 app.use("/users", users);
+*/
 
 app.listen(port, () => {
   console.log("serveur est en ligne");
