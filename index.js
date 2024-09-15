27c35eebad55514e6cef0c237d9b7fbdb888d765
AnandaRAFIDISON <anandarafidison@gmail.com>
2024-09-15 14:00:57 +0300

    update node.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello ");
+const express = require("express");
+const port = 5000;
+
+const app = express();
+
+// GET : POUR RECEPTIONNER LES MESSAGES
+
+app.get("/", (req, res) => {
+  res.send("Hello world avec node.js ");
+});
+
+app.listen(port, () => {
+  console.log("serveur est en ligne");
+});
