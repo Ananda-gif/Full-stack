187ab1cae349caf5df03c9fcacd53876f9001bd3
AnandaRAFIDISON <anandarafidison@gmail.com>
2024-09-15 16:26:15 +0300

    update routes

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const port = 5000;
 
 const app = express();
 
-// GET : POUR RECEPTIONNER LES MESSAGES
-
 app.get("/", (req, res) => {
   res.send("Hello world avec node.js ");
 });
+
+const users = require("./routes/users");
+app.use("/users", users);
 
 app.listen(port, () => {
   console.log("serveur est en ligne");
