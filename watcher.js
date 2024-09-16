fb2a92d7488e94ae7b4f360012427958277918b8
AnandaRAFIDISON <anandarafidison@gmail.com>
2024-09-16 13:39:04 +0300

    update watcher.js

--- watcher.js
+++ watcher.js
@@ -1,5 +1,13 @@
 // PROCESSUS ENFANT comme mettre à jour sans couper et sans mise à jour
-const [node, -, ]
+import { exec } from "node:child_process";
 
+const [node, _, file] = process.argv;
+exec("dir", (error, out, err) => {
+  // exec : EXECUTER
 
-console.log(process.argv);
+  console.log({
+    error,
+    out,
+    err,
+  });
+});
