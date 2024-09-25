8f000c2d155ff6f74fe1806ec2b686a242067d97
AnandaRAFIDISON <anandarafidison@gmail.com>
2024-09-25 15:07:16 +0300

    connexion au base de données

--- Update/index2.js
+++ Update/index2.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const mysql = require("mysql");
 const port = 5000;
+
 const app = express();
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "node",
+  /*socketPath:
+    "WINDOWS CREATORFull-stackUpdate\node_modulesmysqllibConnection.js)",*/
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion : " + err.stack);
+    return;
+  }
+  console.log("Connecté à la base de données MySQL.");
+});
+
+/*--------------------------------------------------------------*/
 // les anciennes commandes que je peut utiliser
 /*
 app.get("/", (req, res) => {
@@ -12,6 +31,7 @@ app.get("/", (req, res) => {
 const contact = require("./routes/contact");
 app.use("/contact", contact);
 */
+/* --------------------------------------------------------------*/
 
 app.listen(port, () => {
   console.log("serveur en ligne et opérationnelle");
